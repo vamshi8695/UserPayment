7967db891d82f018a03dedad97bbf528a7a94a21
Vamshi Mannem <vamshireddymannem@gmail.com>
2025-05-01 18:03:48 -0400

    log added in put payment

--- src/main/java/com/crm/crm_app/controller/PaymentController.java
+++ src/main/java/com/crm/crm_app/controller/PaymentController.java
@@ -3,6 +3,7 @@ package com.crm.crm_app.controller;
 import com.crm.crm_app.dto.PaymentDTO;
 import com.crm.crm_app.model.Payment;
 import com.crm.crm_app.service.PaymentService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import java.util.List;
 
 @RestController  // This tells Spring to handle HTTP requests here
 @RequestMapping("/api/payments")  // Base path for all payment APIs
+@Slf4j
 public class PaymentController {
 
     @Autowired  // Inject the PaymentService
@@ -18,12 +20,14 @@ public class PaymentController {
     // Create a new Payment
     @PostMapping
     public Payment createPayment(@RequestBody PaymentDTO paymentDTO) {
+        log.info("payment is being processed");
         return paymentService.createPayment(paymentDTO);
     }
 
     // Get Payment by ID
     @GetMapping("/{id}")
     public Payment getPaymentById(@PathVariable Long id) {
+        log.info("payment is being made");
         return paymentService.getPaymentById(id);
     }
 
